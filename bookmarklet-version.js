0b5229a686f5e618a79fb291b2bd5bd8eebd498d
JMVRy <77213524+JMVRy@users.noreply.github.com>
2023-03-28 16:07:24 -0500

    Add the main JavaScript files
    These are the JavaScript files that you just make into a bookmarklet. Doesn't work on websites with certain CORS policies

---
+++ bookmarklet-version.js
@@ -0,0 +1,1 @@
+(async () => {const response = await fetch("https://api.weather.gov/gridpoints/ILX/47,91/forecast");if (response.ok) {const jsonValue = await response.json();return Promise.resolve(jsonValue);}else{return Promise.reject("ERROR");}})().then((data) => alert(data["properties"]["periods"][0]["detailedForecast"])).catch(console.error)
